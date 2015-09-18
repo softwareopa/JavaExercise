1d441d81bce1f95ff88c8371b7c0057c841c8639
Ramon Schalleck <ramon_schalleck@hotmail.com>
2015-09-18 12:26:44 +0200

    added class Concert

---
+++ src/Concert.java
@@ -0,0 +1,14 @@
+
+public class Concert {
+ // create a chorus
+ // let them perform a song
+ // create an object hierarchy representing
+ // six objects
+ // class extensions
+ // tenor, extends singer, extends person
+ // members and candidates
+ // perform is writing some lyrics in the output
+ // println(Ramon : "Well you rock my soul in the bossom of abrahma."
+ // println(THorsten: "Well you rock my soul in the bossom of abrahma."
+	
+}
